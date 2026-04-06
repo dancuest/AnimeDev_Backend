8d7a3ffd7493acfd9d608d875bef9fcdc4dcb7b1
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2026-04-05 19:30:00 -0500

    Pruebas del motor de recomendacion

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AnimeModule } from '../anime/anime.module';
 import { InteractionsModule } from '../interactions/interactions.module';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 
 @Module({
   imports: [PrismaModule, AnimeModule, InteractionsModule],
